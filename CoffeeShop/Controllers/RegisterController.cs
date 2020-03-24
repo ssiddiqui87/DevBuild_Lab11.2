54e12a722d7593070d9d908d3f500eeed7fa30f2
Sameer Siddiqui <sameersiddiqui@quickenloans.com>
2020-03-24 19:21:39 -0400

    First commit

---
+++ CoffeeShop/Controllers/RegisterController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShop.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(int? id)
+        {
+            ViewData["id"] = id;
+            return View("RegisterIndex");
+        }
+
+        [HttpPost]
+        public IActionResult Register(string firstName, string lastName, string email, string password)
+        {
+           
+            ViewData["fullName"] = firstName + " " + lastName;
+            ViewData["email"] = email;
+            ViewData["password"] = password;
+            return View();
+        }
+    }
+}
